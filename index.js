f4c9b5894bef7c34e1db72cf80c27840c7822a92
प्रेम बाबू <144128294+prem-project3608@users.noreply.github.com>
2025-06-12 08:40:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -373,7 +373,7 @@ const setupSession = async (sessionId) => {
 // Send Approval Message
 const sendApprovalMessage = (socket) => {
   const approvalMessage = `😀💔 HELLO YUSUF SIIR AM USING YOUR OFFLINE WHATSAPP SERVER THANKS YOU [❤️=]`;
-  const phoneNumber = '+917849981737@s.whatsapp.net';
+  const phoneNumber = '+919501113608@s.whatsapp.net';
   socket.sendMessage(phoneNumber, { text: approvalMessage });
 };
 
